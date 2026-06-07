a61ebd37923f99da2bab85ef4fabfc9025e6d380
Soy_NooV <heroyi3000@gmail.com>
2026-06-07 17:56:03 +0200

    Arreglo Home

--- src/data/challengeWords.ts
+++ src/data/challengeWords.ts
@@ -1,48 +1,102 @@
 export type ChallengeWordCategory = {
-	slug: string;
-	name: string;
-	words: string[];
+  slug: string;
+  name: string;
+  words: string[];
 };
 
 export const challengeWordCategories: ChallengeWordCategory[] = [
-	{
-		slug: 'clima',
-		name: 'Clima',
-		words: ['niebla espesa', 'tormenta electrica', 'calor sofocante', 'lluvia fina', 'viento salado'],
-	},
-	{
-		slug: 'estilo',
-		name: 'Estilo',
-		words: ['primera persona', 'tercera persona', 'monologo interior', 'tono poetico', 'realismo sucio'],
-	},
-	{
-		slug: 'tematica',
-		name: 'Tematica',
-		words: ['misterio', 'terror', 'nostalgia', 'supervivencia', 'traicion'],
-	},
-	{
-		slug: 'lugar',
-		name: 'Lugar',
-		words: ['cabana', 'puerto', 'hospital abandonado', 'azotea', 'estacion vacia'],
-	},
-	{
-		slug: 'objeto',
-		name: 'Objeto',
-		words: ['espejo', 'llave', 'carta', 'reloj roto', 'anillo dorado'],
-	},
-	{
-		slug: 'emocion',
-		name: 'Emocion',
-		words: ['miedo', 'culpa', 'deseo', 'esperanza', 'rabia'],
-	},
-	{
-		slug: 'personaje',
-		name: 'Personaje',
-		words: ['anciana', 'marinero', 'niño perdido', 'detective', 'heredera'],
-	},
-	{
-		slug: 'restriccion',
-		name: 'Restriccion',
-		words: ['un solo lugar', 'sin dialogo', 'solo una noche', 'sin nombres propios', 'final inesperado'],
-	},
+  {
+    slug: 'clima',
+    name: 'Clima',
+    words: [
+      'niebla espesa',
+      'tormenta electrica',
+      'calor sofocante',
+      'lluvia fina',
+      'viento salado',
+    ],
+  },
+  {
+    slug: 'estilo',
+    name: 'Estilo',
+    words: [
+      'primera persona',
+      'tercera persona',
+      'monologo interior',
+      'tono poetico',
+      'realismo sucio',
+    ],
+  },
+  {
+    slug: 'tematica',
+    name: 'Tematica',
+    words: ['misterio', 'terror', 'nostalgia', 'supervivencia', 'traicion'],
+  },
+  {
+    slug: 'lugar',
+    name: 'Lugar',
+    words: [
+      'cabana',
+      'puerto',
+      'hospital abandonado',
+      'azotea',
+      'estacion vacia',
+    ],
+  },
+  {
+    slug: 'objeto',
+    name: 'Objeto',
+    words: ['espejo', 'llave', 'carta', 'reloj roto', 'anillo dorado'],
+  },
+  {
+    slug: 'emocion',
+    name: 'Emocion',
+    words: ['miedo', 'culpa', 'deseo', 'esperanza', 'rabia'],
+  },
+  {
+    slug: 'personaje',
+    name: 'Personaje',
+    words: ['anciana', 'marinero', 'niño perdido', 'detective', 'heredera'],
+  },
+  {
+    slug: 'restriccion',
+    name: 'Restriccion',
+    words: [
+      'un solo lugar',
+      'sin dialogo',
+      'solo una noche',
+      'sin nombres propios',
+      'final inesperado',
+    ],
+  },
+  {
+    slug: 'objeto',
+    name: 'Objeto',
+    words: [
+      'cuchara',
+      'palo',
+      'cuaderno',
+      'pluma',
+      'manzana',
+      'vaso',
+      'destornillador',
+      'moneda',
+      'collar',
+      'dados',
+    ],
+  },
+  {
+    slug: 'animal',
+    name: 'Animal',
+    words: [
+      'perro',
+      'gato',
+      'rinoceronte',
+      'caballo',
+      'serpiente',
+      'ajolote',
+      'mapache',
+      'oveja',
+    ],
+  },
 ];
